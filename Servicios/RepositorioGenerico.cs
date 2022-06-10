450175e85dfc5991421ad627c150857687f91ee7
Jeremias Santana <santanajere@gmail.com>
2022-06-10 00:20:30 -0400

    Carpeta servicios creada y configurada en el lugar correspondiente.

---
+++ Servicios/RepositorioGenerico.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using FormularioGenerico1._5.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormularioGenerico1._5.Servicios
+{public interface IRepositorioGenerico{
+        void FormulariadoGenerizado(FormularioGenerico formularioGenerico);
+    }
+        public class RepositorioGenerico : IRepositorioGenerico
+    { 
+        private readonly string connectionsStrings;
+
+        public RepositorioGenerico(IConfiguration configuration)
+        {
+            connectionsStrings = configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        public void FormulariadoGenerizado(FormularioGenerico formularioGenerico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FormularioGenerico(FormularioGenerico formularioGenerico)
+        {
+              using (var conexion = new SqlConnection(connectionsStrings))
+            {
+                conexion.Query("$@fcinsert into FormularioGenerico" +
+                     "(nombre_usuario, apellido_usuario, sexo_usuario, fecha_usuario, edad_usuario)" +
+                     "values" +
+                     "(@nombre_usuario, @apellido_usuario, @sexo_usuario, @fecha_usuario, @edad_usuario) ", formularioGenerico);
+
+            }
+        }
+
+    }
+
+}
